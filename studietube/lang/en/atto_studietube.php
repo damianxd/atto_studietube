f8c0c080634fd83eefe23830fa07ec86f0b332fd
Damian Alarcon <damian@nikai.cl>
2021-11-19 08:17:04 -0300

    Update
    Update to 2.0.0

---
+++ studietube/lang/en/atto_studietube.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto mod for Lærit.dk
+ *
+ * @package    atto_studietube
+ * @copyright  2019 Damian Alarcon
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Import media from Studietube';
+$string['studietube'] = 'Studietube';
+$string['size'] = 'Size';
+$string['saveimage'] = 'Save';
+$string['fullsize'] = 'Fullsize';
+$string['stsize'] = 'Media properties';
+$string['width'] = 'Width';
+$string['height'] = 'Height';
+$string['privacy:metadata'] = 'The Lærit plugin does not store any personal data.';
