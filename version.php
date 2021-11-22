219e461da43788b301add2a563df0acf3f7df96a
Damian Alarcon <damian@nikai.cl>
2021-11-22 07:46:53 -0300

    Update version.php

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021111900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021112200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires Moodle 2.7 or higher, when Atto was added to core.
 $plugin->component = 'atto_studietube';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array('filter_studietube' => 2021111900);
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.0.0'; // Human readable version information.
+$plugin->release   = '2.0.1'; // Human readable version information.
